7a3d1778ba038035dc2c1eedbae8409a26fa88c6
PezaAntonio <pezaantonio@gmail.com>
2021-10-09 13:28:23 -0700

    initial commit

---
+++ C195Software2/src/DAO/DatabaseConnection.java
@@ -0,0 +1,21 @@
+package DAO;
+
+public class DatabaseConnection {
+
+    private String server;
+    private String username;
+    private String password;
+    private Connection connection;
+
+    public void connect(){
+
+    }
+
+    public void disconnect(){
+
+    }
+
+    public Connection getConnection(){
+
+    }
+}
---
+++ C195Software2/src/model/Customer.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Customer {
+
+    private int customerID;
+    private String customerName;
+    private String customerAddress;
+    private String customerCity;
+    private String customerZip;
+    private String customerPhone;
+
+    public Customer(int customerID, String customerName, String customerAddress, String customerCity, String customerZip, String customerPhone){
+
+    }
+}
