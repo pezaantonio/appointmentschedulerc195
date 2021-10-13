c5a225646e676274266e6dcda786fee792ffa989
Antonio Peza <56060301+pezaantonio@users.noreply.github.com>
2021-10-13 03:34:35 +0000

    Added file from VM
    updated main to include DB connection

--- C195Software2/src/main/Main.java
+++ C195Software2/src/main/Main.java
@@ -1,10 +1,16 @@
 package main;
 
+import DAO.DatabaseConnection;
+import DAO.DatabaseQuery;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main extends Application {
 
@@ -17,7 +23,10 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        DatabaseConnection.openConnection();
+
+        DatabaseConnection.closeConnection();
         launch(args);
     }
 }
